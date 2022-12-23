5c36a196ba1c6db304b84acf35eaea0c97f05d86
radenrishwan <radenrishwan@gmail.com>
2022-12-23 23:08:06 +0700

    features: add upcoming, trending, and top rated movies

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:developer';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:crack_the_roll/common/constant.dart';
+import 'package:crack_the_roll/data/model/short_movie.dart';
+import 'package:crack_the_roll/data/service/movie_service.dart';
 import 'package:crack_the_roll/widget/carousel_card.dart';
+import 'package:crack_the_roll/widget/movie_card.dart';
 import 'package:crack_the_roll/widget/title_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -10,34 +15,32 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size(double.infinity, kToolbarHeight + 50),
-        child: Padding(
-          padding: kLargePadding,
-          child: AppBar(
-            title: const Text('Crack The Roll.'),
-            titleSpacing: 0,
-            actions: [
-              Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: kDefaultBorderRadius,
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.search,
-                    size: 28,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Padding(
+              // TODO: implement sliver app bar
+              padding: kLargePadding,
+              child: AppBar(
+                title: const Text('Crack The Roll.'),
+                titleSpacing: 0,
+                actions: [
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: kDefaultBorderRadius,
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                        size: 28,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Padding(
               padding: kLargePadding.copyWith(top: 0),
               child: TitleWidget(
@@ -46,18 +49,43 @@ class HomeScreen extends StatelessWidget {
                 iconColor: kPrimaryColor,
               ),
             ),
-            CarouselSlider.builder(
-              itemCount: 5,
-              options: CarouselOptions(
-                autoPlay: true,
-                enlargeCenterPage: true,
-                height: 280,
-                enlargeStrategy: CenterPageEnlargeStrategy.zoom,
-              ),
-              itemBuilder: (context, index, realIndex) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return const CarouselCard();
+            FutureBuilder<List<ShortMovie>>(
+              future: MovieService().getUpcoming(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  log(snapshot.error.toString());
+                  return const Center(
+                    child: Text('Something went wrong'),
+                  );
+                }
+
+                if (snapshot.connectionState != ConnectionState.done) {
+                  return const SizedBox(
+                    height: 280,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+
+                final movies = snapshot.data!;
+
+                return CarouselSlider.builder(
+                  itemCount: movies.length,
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                    height: 280,
+                    enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+                  ),
+                  itemBuilder: (context, index, realIndex) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return CarouselCard(
+                          movie: movies[index],
+                        );
+                      },
+                    );
                   },
                 );
               },
@@ -82,18 +110,107 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-            ...List<Widget>.generate(
-              5,
-              (index) {
-                return Padding(
-                  padding: kLargePadding.copyWith(top: 0, bottom: kDefaultPadding.bottom),
-                  child: Card(
-                    child: Stack(
+            FutureBuilder<List<ShortMovie>>(
+              future: MovieService().getTrending(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  log(snapshot.error.toString());
+                  return SizedBox(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    child: const Center(
+                      child: Text('Something went wrong'),
+                    ),
+                  );
+                }
+
+                if (snapshot.connectionState != ConnectionState.done) {
+                  return SizedBox(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    child: const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+
+                final result = snapshot.data!;
+
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Padding(
+                    padding: kLargePadding.copyWith(top: 0, bottom: kDefaultPadding.bottom),
+                    child: Row(
                       children: [
-                        Container(
-                          height: 100,
-                          width: MediaQuery.of(context).size.width,
-                          color: Colors.red,
+                        ...List<Widget>.generate(
+                          result.length,
+                          (index) {
+                            return MovieCard(movie: result[index]);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            Padding(
+              padding: kLargePadding,
+              child: TitleWidget(
+                title: 'Top Rated',
+                icon: Icons.star,
+                iconColor: kPrimaryColor,
+                trailing: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'See All',
+                      style: TextStyle(
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            FutureBuilder<List<ShortMovie>>(
+              future: MovieService().getTopRated(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  log(snapshot.error.toString());
+                  return SizedBox(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    child: const Center(
+                      child: Text('Something went wrong'),
+                    ),
+                  );
+                }
+
+                if (snapshot.connectionState != ConnectionState.done) {
+                  return SizedBox(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    child: const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+
+                final result = snapshot.data!;
+
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Padding(
+                    padding: kLargePadding.copyWith(top: 0, bottom: kDefaultPadding.bottom),
+                    child: Row(
+                      children: [
+                        ...List<Widget>.generate(
+                          result.length,
+                          (index) {
+                            return MovieCard(movie: result[index]);
+                          },
                         ),
                       ],
                     ),
