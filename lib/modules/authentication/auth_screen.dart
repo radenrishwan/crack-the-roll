532e1ad076dd813ba9c3d849704e4d9a409264e4
radenrishwan <radenrishwan@gmail.com>
2022-12-31 10:57:54 +0700

    add movies list on login screen

--- lib/modules/authentication/auth_screen.dart
+++ lib/modules/authentication/auth_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:developer';
+
 import 'package:crack_the_roll/common/constant.dart';
 import 'package:crack_the_roll/common/route.dart';
+import 'package:crack_the_roll/data/model/short_movie.dart';
+import 'package:crack_the_roll/data/service/movie_service.dart';
 import 'package:crack_the_roll/widget/register_widget.dart';
 import 'package:crack_the_roll/widget/default_alert.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -42,7 +47,61 @@ class AuthScreen extends StatelessWidget {
                           letterSpacing: 1.4,
                         ),
                   ),
-                  // TODO: show most popular movies
+                  SizedBox(height: kDefaultPadding.top),
+                  Padding(
+                    padding: kLargePadding.copyWith(top: 0, bottom: 0),
+                    child: FutureBuilder<List<ShortMovie>>(
+                      future: MovieService().getTrending(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          log(snapshot.error.toString());
+                          return SizedBox(
+                            height: 100,
+                            width: MediaQuery.of(context).size.width,
+                            child: const Center(
+                              child: Text('Something went wrong'),
+                            ),
+                          );
+                        }
+
+                        if (snapshot.connectionState != ConnectionState.done) {
+                          return SizedBox(
+                            height: 100,
+                            width: MediaQuery.of(context).size.width,
+                            child: const Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
+                        }
+
+                        final result = snapshot.data!;
+
+                        return SizedBox(
+                          height: 160,
+                          child: ListView.builder(
+                            itemCount: 8,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: kSmallPadding.copyWith(top: 0, bottom: 0),
+                                child: ClipRRect(
+                                  borderRadius: kDefaultBorderRadius,
+                                  child: FancyShimmerImage(
+                                    imageUrl: 'https://image.tmdb.org/t/p/w200/${result[index].posterPath}',
+                                    boxFit: BoxFit.cover,
+                                    width: 120,
+                                    shimmerBaseColor: kPrimaryColor,
+                                    shimmerHighlightColor: kSecondaryColor,
+                                    shimmerBackColor: kLightBackgroundColor,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                   const Spacer(flex: 2),
                   Padding(
                       padding: kLargePadding,
